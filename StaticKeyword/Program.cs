8f519252a0ed1ccccb89903759e87888d214a4f4
beverley-dick <beverleydick11@gmail.com>
2023-05-08 17:02:44 +0300

    Add project files.

---
+++ StaticKeyword/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StaticKeyword
+{
+    class Student
+    {
+
+        //static  variable 
+        public static string department = "Computer Science";
+
+        
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //access static variable 
+
+            Console.WriteLine("Department: " + Student.department);
+
+            Console.ReadLine();
+        }
+    }
+}
